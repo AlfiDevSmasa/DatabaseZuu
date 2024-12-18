96e2ca822f1a4ca23d0d95b277ba70fc1a76d1e3
AlfiDevSmasa <reniandin94@gmail.com>
2024-12-18 13:07:15 +0700

    Create qwerty.js

---
+++ qwerty.js
@@ -0,0 +1,10 @@
+[
+  "6283890875133@s.whatsapp.net",
+  "6283840095883@s.whatsapp.net",
+  "6287872921691@s.whatsapp.net",
+  "6283173488914@s.whatsapp.net",
+  "6287779469491@s.whatsapp.net",
+  "60163288976@s.whatsapp.net",
+  "60183218397@s.whatsapp.net",
+  "6285210172194@s.whatsapp.net"
+]
